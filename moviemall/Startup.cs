2f1b0d7dcc73c7273e7e1adbaf872d6cb1986885
HeRann <hemal.m.rana@outlook.com>
2017-12-06 18:25:28 -0500

    Add project files.

---
+++ moviemall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(moviemall.Startup))]
+namespace moviemall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
