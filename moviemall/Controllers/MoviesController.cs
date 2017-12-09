af5f176515cc2b62d4332cb8c05354f462c6b776
HeRann <hemal.m.rana@outlook.com>
2017-12-09 16:57:42 -0500

    Day2-12-9-17 (More methods added)

--- moviemall/Controllers/MoviesController.cs
+++ moviemall/Controllers/MoviesController.cs
@@ -15,6 +15,34 @@ namespace moviemall.Controllers
             var movie = new Movie();
             movie.Name = "Jurassic World";
             return View(movie);
+            //return Content("Hello World!"); // This will only return string to the view 
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home",new { page = 1, sortBy = "name" });
+            //return RedirectToAction("Contact", "Home",new { page = 1 });
         }
+
+        //public ActionResult Edit(int idc)
+        //{
+        //    return Content("Value is = " + idc);
+
+        //}// It must be query string: http://localhost:57447/movies/edit?idc=1
+
+        public ActionResult Edit(int id)
+        {
+            //return Content(String.Format("Value is = {0}" , id));
+            return Content("Value is = {0}"+ id);
+
+        }
+
+        //Nevigate to movies
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
+
+        //}
     }
 }
